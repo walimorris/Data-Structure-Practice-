9a48d8589b661d437fcfec78676fee2686d369c3
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-08-28 10:10:06 -0700

    Update Employee.java

--- BinaryTree/src/com/morris/datastructures/Employee.java
+++ BinaryTree/src/com/morris/datastructures/Employee.java
@@ -1,5 +1,15 @@
 package com.morris.datastructures;
 
+/** 
+ * This is an employee class that we'll eventually use with a EmployeeTree. Each Employee has an id number which references a job title. 
+ * The EmployeeTree will use the id to find out what is the job of the Employee it's looking for. We could even expand more on this and 
+ * search for a specific job title to look for an Employee that fits into our desired job search. Employee will be expanded to include the
+ * Employee's name, e-mail, and technologies. There's endless possibilities, so let's just mess around. 
+ * 
+ * @author Wali Morris
+ * @since 08/30/2020
+ */ 
+
 public class Employee {
     private int id;
     private String job;
