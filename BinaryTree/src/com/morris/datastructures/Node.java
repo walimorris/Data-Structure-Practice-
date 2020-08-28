580e28df99e8681f13a72f6093a5d039ed0f3c50
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-08-28 10:11:57 -0700

    Update Node.java

--- BinaryTree/src/com/morris/datastructures/Node.java
+++ BinaryTree/src/com/morris/datastructures/Node.java
@@ -1,5 +1,12 @@
 package com.morris.datastructures;
 
+/**
+ * A generic Node class which contains a int key and data of type String(could be anything). 
+ * 
+ * @author Wali Morris
+ * @since 08/30/2020
+ */ 
+
 public class Node {
     int key;
     String data;
